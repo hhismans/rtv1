e423e091dab120970043e4a2ec3e89523294e9d0
Hugo HISMANS <hhismans@e1r13p5.42.fr>
2016-12-01 14:49:12 +0100

    eclipse

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hhismans <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 16:37:06 by hhismans          #+#    #+#             */
-/*   Updated: 2016/11/29 12:49:22 by hhismans         ###   ########.fr       */
+/*   Updated: 2016/12/01 14:31:04 by hhismans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		init_mlx(t_env *e)
 		printf("error mlx init\n");
 		exit(0);
 	}
-	//mlx_key_hook(e->win, key_hook, e);
+	mlx_key_hook(e->win, key_hook, e);
 	//mlx_mouse_hook(e->win, mouse_hook, e);
 	mlx_expose_hook(e->win, expose, e);
 	//mlx_hook(e->win, MOTION_NOTIFY, PTR_MOTION_MASK, motion_hook, e);
@@ -47,28 +47,65 @@ void	init_env(t_env *e)
 	cam_init(e->cam);
 // a changer
 	t_data	data;
-	data.color = BLUE;
-	data.data[0] = 2;
-	data.type = SPHERE;
-	set_vector(&data.vector, 0,0,3);
 // changer
+	data.type = SPHERE;
+	data.color =  0x41698B;
+	data.data[0] = 4;
+	data.data[1] = 1;
+	set_vector(&data.vector, 0,0,10);
 	e->objs = new_obj(data);
-/*	data.type = SPHERE;
-	data.color = RED;
-	data.data[0] = 2;
-	set_vector(&data.vector, 0,2,3);
+
+	data.color = 0xffffff;
+	data.data[0] = 0.5;
+	data.data[1] = 1;
+	set_vector(&data.vector, 6,6,-2);
 	pushback_obj(e->objs, data);
 
+/*	data.color = 0x41698B;
+	data.data[1] = 1;
+	set_vector(&data.vector, 0,4,10);
+	pushback_obj(e->objs, data);
+
+	data.color = 0x10f08a;
+	data.data[1] = 0;
+	set_vector(&data.vector, 0,-4,10);
+	pushback_obj(e->objs, data);
+
+	data.color = 0xffff00;
+	data.data[1] = 0;
+	set_vector(&data.vector, -4,0,10);
+	pushback_obj(e->objs, data);*/
+
+/*	data.color = 0xffffff;//1493;
+	data.data[0] = 0.5;
+	data.data[0] = 1;
 	data.type = SPHERE;
+	set_vector(&data.vector, 5,5,0);
+	pushback_obj(e->objs, data);*/
+
+	/*data.type = SPHERE;
 	data.color = 0x00ff00;
-	data.data[0] = 2;
-	set_vector(&data.vector, 2.5,4,3);
+	data.data[0] = 3;
+	set_vector(&data.vector, 3,0,8);
+	pushback_obj(e->objs, data);
+
+	data.color = 0xffff00;
+	set_vector(&data.vector, 0,3,8);
+	pushback_obj(e->objs, data);
+
+	data.color = 0x00868b;
+	set_vector(&data.vector, 0,-3,8);
 	pushback_obj(e->objs, data);*/
 
 	data.type = POINT_LIGHT;
-	data.color = WHITE;
-	set_vector(&data.vector, 0,0,0);
+	data.color = 0xffffff;
+	set_vector(&data.vector, 10,10,-10);
 	e->lights = new_obj(data);
+	//set_vector(&data.vector, -10,-10,10);
+	//pushback_obj(e->lights,data);
+	//data.color = 0xff;
+	//set_vector(&data.vector, 0,0,-10);
+	//pushback_obj(e->lights,data);
 }
 
 void	mlx_pixel_put_img(void *img_ptr, int x, int y, int color)
